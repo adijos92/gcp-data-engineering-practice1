eba2fb3e00d094e4e04586d0209a867a693b34e0
gcp-project-demo <adityajoshi1292@gmail.com>
2026-06-02 11:29:09 +0530

    Filter orders above 5000

--- data/dataproc/orders_etl.py
+++ data/dataproc/orders_etl.py
@@ -1,12 +1,18 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
 
-spark = SparkSession.builder.appName(
-    "orders_etl"
-).getOrCreate()
+spark = SparkSession.builder.appName("orders_etl").getOrCreate()
 
 df = spark.read.csv(
     "gs://orders-landing-bucket-demo/orders.csv",
-    header=True
+    header=True,
+    inferSchema=True
 )
 
+print("Original Orders:")
 df.show()
+
+filtered_df = df.filter(col("amount") > 5000)
+
+print("Orders above 5000:")
+filtered_df.show()
